d4ebaeaf97f45326a188af6c22c3e2e2b3c05efc
Thiago Pizzutti <tpizzutti@gmail.com>
2020-01-24 13:15:56 -0300

    functionality ok

--- app.js
+++ app.js
@@ -13,7 +13,17 @@ loadEventListeners();
 function loadEventListeners() {
   //add task event
   form.addEventListener("submit", addTask);
+
+  //Remove task event
+  taskList.addEventListener("click", removeTask);
+
+  //Clear tasks
+  clearBtn.addEventListener("click", clearTasks);
+
+  //Filter tasks
+  filter.addEventListener("keyup", filterTasks);
 }
+
 // Add task
 function addTask(e) {
   if (taskInput.value === "") {
@@ -37,6 +47,38 @@ function addTask(e) {
   taskList.appendChild(li);
   //Clear Input
   taskInput.value = "";
-
   e.preventDefault();
+}
+
+//Remove Task
+
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    if (confirm("Are you sure")) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+//Clear Tasks
+
+function clearTasks(e) {
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+// Filter tasks
+
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll(".collection-item").forEach(function(task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
 }
